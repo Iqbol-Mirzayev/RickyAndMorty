21bf08e523447104ce35beb753ba90eff8735149
Iqbol-Mirzayev <you@example.com>
2023-06-30 21:44:38 +0500

    foldering and add repo

---
+++ lib/future/domain/repositories/person_repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:ricky_and_morty/core/error/failure.dart';
+import 'package:ricky_and_morty/future/domain/entities/person_entity.dart';
+
+abstract class PersonRepository {
+  Future<Either<Failure, List<PersonEntity>>> getAllPersons(int page);
+  Future<Either<Failure, List<PersonEntity>>> searchPerson(String query);
+}
+  
---
+++ lib/future/domain/usecases/get_all_persons.dart
@@ -0,0 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:ricky_and_morty/core/usecases/usecase.dart';
+import 'package:ricky_and_morty/future/domain/repositories/person_repository.dart';
+import 'package:meta/meta.dart';
+import '../../../core/error/failure.dart';
+import '../entities/person_entity.dart';
+
+class GetAllPersons extends Usecase<List<PersonEntity>, PagePersonParams> {
+  final PersonRepository personRepository;
+  GetAllPersons(this.personRepository);
+
+  Future<Either<Failure, List<PersonEntity>>> call(
+      PagePersonParams params) async {
+    return await personRepository.getAllPersons(params.page);
+  }
+}
+
+class PagePersonParams extends Equatable {
+  final int page;
+  PagePersonParams({required this.page});
+
+  @override
+  List<Object?> get props => [page];
+}
