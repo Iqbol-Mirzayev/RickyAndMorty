8d5359e7e3e05d256ad8b501b7e4140027f0bf0d
Iqbol-Mirzayev <you@example.com>
2023-07-19 21:30:48 +0500

    PersonLisrCubit added

---
+++ lib/future/presentation/bloc/person_list_cubit/cubit/person_list_cubit.dart
@@ -0,0 +1,49 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:ricky_and_morty/future/domain/usecases/get_all_persons.dart';
+
+import '../../../../../core/error/failure.dart';
+import '../../../../domain/entities/person_entity.dart';
+
+part 'person_list_state.dart';
+
+const SERVER_FAILURE_MESSAGE = 'Server Failure';
+const CACHED_FAILURE_MESSAGE = 'Cache Failure';
+
+class PersonListCubit extends Cubit<PersonState> {
+  final GetAllPersons getAllPersons;
+
+  PersonListCubit({required this.getAllPersons}) : super(PersonEmpty());
+
+  int page = 1;
+
+  void loadPerson() async {
+    if (state is PersonLoading) return;
+    final currentState = state;
+    var oldPerson = <PersonEntity>[];
+    if (currentState is PersonLoaded) {
+      oldPerson = currentState.personsList;
+    }
+    emit(PersonLoading(oldPerson, isFirstFetch: page == 1));
+    final failureOrPerson = await getAllPersons(PagePersonParams(page: page));
+    failureOrPerson
+        .fold((error) => PersonError(message: _mapFailureToMessage(error)),
+            (character) {
+      page++;
+      final persons = (state as PersonLoading).oldPersonsList;
+      persons.addAll(character);
+      emit(PersonLoaded(persons));
+    });
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case CacheFailure:
+        return CACHED_FAILURE_MESSAGE;
+      default:
+        return 'UnExpected Error';
+    }
+  }
+}
--- lib/future/presentation/bloc/search_bloc/search_bloc.dart
+++ lib/future/presentation/bloc/search_bloc/search_bloc.dart
@@ -2,11 +2,13 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:ricky_and_morty/core/error/failure.dart';
 import 'package:ricky_and_morty/future/domain/entities/person_entity.dart';
-
-import '../../../../domain/usecases/search_person.dart';
+import '../../../domain/usecases/search_person.dart';
 
 part 'search_event.dart';
 part 'search_state.dart';
+
+const SERVER_FAILURE_MESSAGE = 'Server Failure';
+const CACHED_FAILURE_MESSAGE = 'Cache Failure';
 
 class PersonSearchBloc extends Bloc<PersonSearchEvent, PersonSearchState> {
   final SearchPerson searchPerson;
@@ -33,9 +35,9 @@ class PersonSearchBloc extends Bloc<PersonSearchEvent, PersonSearchState> {
   String _mapFailureToMessage(Failure failure) {
     switch (failure.runtimeType) {
       case ServerFailure:
-        return 'Server Failure';
+        return SERVER_FAILURE_MESSAGE;
       case CacheFailure:
-        return 'Cache Failure';
+        return CACHED_FAILURE_MESSAGE;
       default:
         return 'UnExpected Error';
     }
